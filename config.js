1f3785fbd4b703224bbfced6d08e13dcdbe9d92e
samer khallouf <80248505+samerkhallouf@users.noreply.github.com>
2021-12-06 11:13:05 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb+srv://admin:admin@cluster0.k9zpb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
